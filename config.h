d2e1e3aac50ea51ebcfe4e97e9b0cb03360acb86
christophersoo <104320816+christophersoo@users.noreply.github.com>
2025-03-26 20:34:34 +0000

    added more functions in prototype, going for dinner break.

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define DIM 2
+#define DIM 3
 #define N 100
 #define SWEEP 10000
 #define CONFS 1000000
--- prototype.cpp
+++ prototype.cpp
@@ -1,12 +1,54 @@
 #include <iostream>
+#include "config.h"
 #include <bitset>
 #include <random>
 using namespace std;
 
-int main(){
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<int> dist(0, 1);
+constexpr int power(int base, int exp) {return (exp == 0) ? 1 : base * power(base, exp - 1);}
+constexpr int LEN = power(N, DIM);
 
-    dist(gen);
+bitset<LEN> state;
+random_device rd;
+mt19937 gen(rd());
+uniform_int_distribution<int> dist(0, LEN);
+
+int energy_tot(bitset<LEN>& state){
+    int energy = 0;
+    for (int i=0; i<LEN-1; i++){
+        energy += -(4 * state[i] * state[i+1] - 2 * state[i] - 2 * state[i+1] + 1);
+    }
+    return energy;
+}
+
+int magnet_tot(bitset<LEN>& state){
+    int magnetisation = 0;
+    for (int i=0; i<LEN; i++){
+        magnetisation += 2 * state[i] - 1;
+    }
+}
+
+int get(bitset<LEN>& state, int idx_arr[]){
+    int idx = 0, factor = 1;
+    for (int i=DIM-1; i>=0; i--){idx += idx_arr[i] * factor; factor *= N;}
+    return 2 * state[idx] - 1;
+}
+
+void flip(bitset<LEN>& state, int idx_arr[]){
+    int idx = 0, factor = 1;
+    for (int i=DIM-1; i>=0; i--){idx += idx_arr[i] * factor; factor *= N;}
+    state.flip(idx);
+}
+
+void init(bitset<LEN>& state){
+    for (int i = 0; i<LEN; i++){state[i] = dist(gen) % 2;}
+}
+
+void MC_SWEEP(bitset<LEN>& state){
+    int s_idx = dist(gen);
+    
+}
+
+int main(){
+    init(state);
+    return 0;    
 }
