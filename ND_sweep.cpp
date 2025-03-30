097a63b31a791ccf7d3f9a5a9be5d1e4c99b2e43
christophersoo <104320816+christophersoo@users.noreply.github.com>
2025-03-30 16:47:51 +0000

    added sweep analysis, will add comments, etc after this. Everything is done.

--- ND_sweep.cpp
+++ ND_sweep.cpp
@@ -8,29 +8,24 @@ long long pow_int(long long base, long long exp) {
 }
 
 int main(int argc, char* argv[]){
-    if (argc != 6) {
+    if (argc != 5) {
         return 1;
     }
 
     int N = stoi(argv[1]);
     int DIM = stoi(argv[2]);
-    int SWEEP = stoi(argv[3]);
-    int CONFS = stoi(argv[4]);
-    float BETA = stof(argv[5]);
+    float BETA = stof(argv[3]);
+    int MAX_SWEEP = stoi(argv[4]);
 
     cout << "Initialising System..." << endl;
     ND_System nd_sys(N, DIM, pow_int(N, DIM));
-    ofstream file("output.csv");
-    file << "id,energy_total,magnetisation" << endl;
-
-    for (long long i = 0; i < CONFS; i++) {
-        for (long long j = 0; j < SWEEP; j++) {
-            nd_sys.MC_SWEEP(BETA);
-        }
+    ofstream file("sweeps.csv");
+    file << "sweep,energy,magnetisation" << endl;
+    for (int i = 0; i < MAX_SWEEP; i++){
+        nd_sys.MC_SWEEP(BETA);
         file << i << "," << nd_sys.energy_tot() << "," << nd_sys.magnet_tot() << endl;
     }
-
+    cout << "Sweeps Recorded." << endl;
     file.close();
-    cout << "Successfully Created " << CONFS << " Configurations." << endl;
     return 0;
 }
